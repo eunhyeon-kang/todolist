fa66ae53f841b44f404231acb78307220a124791
eunhyeon-kang <cjswodmsgus@naver.com>
2024-12-16 16:19:19 +0900

    할 일 별 중요도 출력 기능 추가가

--- todolist.py
+++ todolist.py
@@ -6,21 +6,22 @@
 def add_task(task_list):
     task = input("What Add to list?:")
     deadline = input("Enter the deadline : ex) 2001.03.15 ") # 데드라인 확인 가능을 추가
-    task_list.append((task,deadline)) # 데드라인이 같이 보여지게끔 추가
+    importnat = input("Enter the importance : ex) 중요, 보통, 낮음 ") # 중요도 기능 추가함
+    task_list.append((task, deadline, importnat)) # 데드라인이 같이 보여지게끔 추가 + 중요도
     print()  # 줄 바꿈 용 코드
-    print(f"{task}, deadline {deadline} is added. ") # 같이 출력 할 수 있게 함.
+    print(f"{task}, deadline {deadline}, importance {importnat} is added. ") # 같이 출력 할 수 있게 함. + 중요도 출력도 추가함
     print()
 
 # 할 일 제거하기 
 def remove_task(task_list):
     print("To do list")
-    for i, (task, deadline) in enumerate(task_list):
-        print(f"{i+1}.{task} (deadline : {deadline})")
+    for i, (task, deadline, importnat) in enumerate(task_list):
+        print(f"{i+1}. {task} (deadline: {deadline}, importance: {importnat})") # 성능 2개 추가함
     index = int(input("Enter the number you want to remove: ")) -1
     if 0 <= index < len(task_list):
-        removed_task, removed_deadline = task_list.pop(index)
+        removed_task, removed_deadline, removed_importnat = task_list.pop(index)
         print()
-        print(f"{removed_task} (deadline : {removed_deadline}) is removed to list")
+        print(f"{removed_task} (deadline : {removed_deadline}), importance: {removed_importnat}) is removed from the list.")
     else:
         print("Invaild number. Please try again.")
     print()
@@ -28,8 +29,8 @@ def remove_task(task_list):
 # 할 일 확인하기
 def print_list(task_list):
     print("To do list")
-    for i, (task, deadline) in enumerate(task_list):
-        print(f"{i+1}. {task} (deadline : {deadline})")
+    for i, (task, deadline, importnat) in enumerate(task_list):
+        print(f"{i+1}. {task} (deadline: {deadline}, importance: {importnat})") # 두 기능을 다 출력할 수 있게 추가.
     print()
 
 # 실행 코드
